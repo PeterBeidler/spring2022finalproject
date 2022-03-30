effe7a891cf92b18da6135fd5414a00901ef11a6
PeterBeidler <102323163+PeterBeidler@users.noreply.github.com>
2022-03-30 17:31:29 -0400

    Update final_project_preposal.py

--- final_project_preposal.py
+++ final_project_preposal.py
@@ -2,18 +2,18 @@
 
 ## What idea(s) do you have for your final project?
 
-#-Some sort of modern day Oregon Trail
-#-Wordle but for countries
-#-Codebreaker game with more spaces and colors
+Some sort of modern day Oregon Trail
+Wordle but for countries
+Codebreaker game with more spaces and colors
 
 
 ## If you plan to collaborate with one or two classmates, what are their names?
 
-#-Solo Project
+Solo Project
 
 ## Do you have any questions of your own?
 
-#-Did I use github correctly with this? Kind of confused.
+Did I use github correctly with this? Kind of confused.
 
 
 
